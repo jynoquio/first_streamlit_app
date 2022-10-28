344b0946725c2c94f8201885f7136383c3e32446
Javier <116371451+jynoquio@users.noreply.github.com>
2022-10-28 14:20:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ def get_fruity_data(this_fruit_choice):
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
    # write your own comment -what does the next line do? 
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
+   return fruityvice_normalized
 
 # NEw section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
