59cadf0916eeab31c14b057fda41d3f61e1f3408
Javier <116371451+jynoquio@users.noreply.github.com>
2022-10-28 17:34:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,8 +53,6 @@ try:
        streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
-# don't run anything past here while we troubleshoot
-#streamlit.stop()
 
 
 
@@ -79,6 +77,9 @@ if streamlit.button('Get Fruit Load list'):
    my_data_rows = get_fruit_load_list()
    #streamlit.dataframe(my_data_row)
    streamlit.dataframe(my_data_rows)
+
+   # don't run anything past here while we troubleshoot
+streamlit.stop()
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('The user entered ', fruit_choice)
