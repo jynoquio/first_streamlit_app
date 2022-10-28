ec11f4e1d1a322640f877d6f0e461613d904e101
Javier <116371451+jynoquio@users.noreply.github.com>
2022-10-28 14:28:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,17 +56,27 @@ except URLError as e:
 # don't run anything past here while we troubleshoot
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+
+
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 #my_cur.execute("SELECT * from fruit_load_list")
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 #my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
+
 # streamlit.text("Hello from Snowflake:")
 #streamlit.text("The fruit load list contains:")
 #streamlit.text(my_data_row)
 streamlit.header("The fruit load list contains:")
+#snowflake-related functions
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * from fruit_load_list")
+        return my_cur.fetchall()
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load list'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list()
+   
 #streamlit.dataframe(my_data_row)
 streamlit.dataframe(my_data_rows)
 
